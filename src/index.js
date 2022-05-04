7a6974f9206ecc274befa59ecea9e2a0c28d7c11
Mohammad <fakhrolahi@gmail.com>
2022-05-04 23:38:25 +0430

    Style
    Styled main search

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const header = document.querySelector('#header')
 // Change header style when scroll down
 window.addEventListener('scroll', () => {
   let scrollD = window.scrollY
-  if (scrollD >= 10) {
+  if (scrollD >= 1) {
     header.classList.add('header-bg')
   } else {
     header.classList.remove('header-bg')
